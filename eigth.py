9c50efcb8b230dc5ab0e005b3cfbbfab8d539845
RohitZe <rohithsharmaa105@gmail.com>
2026-01-20 13:17:38 +0530

    loop code added

---
+++ eigth.py
@@ -0,0 +1,8 @@
+# loop-->repetion /iteration
+# intro to loops
+i=2
+while(i<=20):
+    print(i)
+    i=i+2
+
+    
--- seventh.py
+++ seventh.py
@@ -13,22 +13,41 @@
 
 # age input user se btao lekin with fn
 
-def percentage_calculator(math,english,hindi):
-    studentmarks=math+english+hindi
-    percentage=(studentmarks/300)*100
-    print(percentage)
+# def percentage_calculator(math,english,hindi):
+#     studentmarks=math+english+hindi
+#     percentage=(studentmarks/300)*100
+#     print(percentage)
 
 
-math=int(input('enter the marks of math? '))
-english=int(input('enter the marks of math? '))
-hindi=int(input('enter the marks of math? '))
+# math=int(input('enter the marks of math? '))
+# english=int(input('enter the marks of math? '))
+# hindi=int(input('enter the marks of math? '))
 
-percentage_calculator(math,english,hindi)
-
-
-
+# percentage_calculator(math,english,hindi)
 
 
 
+#scope
+
+
+#return
+
+# add ,multiply divide subtract % main() call krna hai result
+# percentage calcualtor 3 sub percentage
+
+
+
+def main():
+   x=int(input('enter the number? '))
+   print(square(x))
+
+def square(x):
+    sq=x*x
+    return sq
+
+
+
+
+main()
 
 
